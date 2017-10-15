a0ec14fb9f99f741bd997fa8cf393169136fa70d
Brandon Lee <greatdg@gmail.com>
2017-10-15 09:47:58 +0800

    added View

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { ViewPropTypes, ListView, Dimensions } from 'react-native';
+import { View, ViewPropTypes, ListView, Dimensions } from 'react-native';
 import { chunkArray } from './utils';
 
 class SuperGrid extends Component {
